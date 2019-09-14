53f7add021bf6accc56a2ad29d6bb1c624a52739
ishigen425 <g4.fine.yamanashi@gmail.com>
2019-09-14 12:26:36 +0900

    change readme

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # TLeagu
 
 試合データのcsvファイルの出力は以下のコマンドです。
-現時点では2018-2019シーズンの試合のみ対応しています。
-2019-2020シーズンの試合はデュースなしのルールにまだ対応できていません。
+第一引数でシーズンを表す引数として、"2018"もしくは"2019"を入力してください。
 
 Tleagu_game.py 2018
 
@@ -10,4 +9,58 @@ Tleagu_game.py 2018
 こちらは2019年に登録されている選手のみとなります。
 
 Tleagu_player.py
+
+個人スタッツはまだ取得できません。
+対応する予定です。
+
+それぞれのcsvファイルの説明をします。
+
+Tleagu_game.pyから出力されるファイル
+ファイル名:match_table_{シーズン}.csv
+文字コード:utf-8
+カラム
+match_id:チームマッチのID URLの後方文字列
+date:試合日
+home:ホームのチーム
+away:アウェーのチーム
+home_point:ホームチームの勝利ゲーム数
+away_point:アウェーチームの勝利ゲーム数
+visitors:観客数
+sex:性別
+
+ファイル名:game_table_{シーズン}.csv
+文字コード:utf-8
+match_id:チームマッチのID
+game_id:各マッチのインデックス番号
+home_player1:ホームチームの選手名
+home_player2:ホームチームの選手名（シングルスの場合は未設定）
+away_player1:アウェーチームの選手名
+away_player2:アウェーチームの選手名（シングルスの場合は未設定）
+home_point:ホームチームの獲得ゲーム数
+away_point:アウェーチームの獲得ゲーム数
+
+ファイル名:point_table_{シーズン}.csv
+文字コード:utf-8
+match_id:チームマッチのID
+game_id:各マッチのインデックス番号
+set_id:各セットのインデックス番号
+point_id:各ポイントのインデックス番号
+home_point:ホームチームの獲得ポイント
+away_point:アウェーチームの獲得ポイント
+serve:サーブ権 1:ホームチーム、2:アウェーチーム
+timeout:タイムアウト 1:ホームチームのタイムアウト直後のポイント 2:アウェーチームのタイムアウト直後のポイント
+
+Tleagu_player.pyから出力されるファイル
+ファイル名:player_table.csv
+文字コード:utf-8
+カラム
+season:選手登録されているシーズン
+player_name:選手名
+team_name:所属チーム
+type:戦型
+birthday:生年月日
+height:身長
+birthplace:出身地
+latest_world_rank:最新世界ランク
+highest_world_rank:最高世界ランク
 
--- Tleagu_player.py
+++ Tleagu_player.py
@@ -2,34 +2,46 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import re
+import sys, traceback
 from player_data_func import get_team_link, get_player_data, get_player_link
 
+SEASON = "2019"
+BASIC_URL = "https://tleague.jp"
+
 def main():
-    SEASON = "2019"
-    BASIC_URL = "https://tleague.jp"
     link = get_team_link()
-    player_table = []
     try:
-        for i in link:
-            url = BASIC_URL + i + "player/"
-            season = "?season=" + SEASON
-            player_list_url = get_player_link(url + season)
-            for player_index, player_url in enumerate(player_list_url):
-                player_url = url + player_url 
-                print(player_url)
-                player_data = get_player_data(player_url)
-                player_table.append([
-                    season[len(season)-4:],player_data[0].strip(), i[6:-1], player_data[1].strip(), player_data[2], 
-                    player_data[3], player_data[4].replace("\t", " ").strip(), player_data[5] or 'NULL', player_data[6] or 'NULL'
-                ])
-    except:
-        raise
+        player_table = make_table(link)
+    except Exception as e:
+        t, v, tb = sys.exc_info()
+        print(traceback.format_exception(t,v,tb))
+        print(traceback.format_tb(e.__traceback__))
     finally:
-        player_header = ["season", "player_name", "team_name", "type", "birthday", "height", "birthplace", "latest_world_rank", "highest_world_rank"]
-        with open('./data/player_table.csv' ,'w', encoding='utf-8') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(player_header)
-            writer.writerows(player_table)
+        export_csv(player_table)
+
+def make_table(link):
+    player_table = []
+    for i in link:
+        url = BASIC_URL + i + "player/"
+        season = "?season=" + SEASON
+        player_list_url = get_player_link(url + season)
+        for player_index, player_url in enumerate(player_list_url):
+            player_url = url + player_url 
+            print(player_url)
+            player_data = get_player_data(player_url)
+            player_table.append([
+                season[len(season)-4:],player_data[0].strip(), i[6:-1], player_data[1].strip(), player_data[2], 
+                player_data[3], player_data[4].replace("\t", " ").strip(), player_data[5] or 'NULL', player_data[6] or 'NULL'
+            ])
+
+    return player_table
+
+def export_csv(player_table):
+    player_header = ["season", "player_name", "team_name", "type", "birthday", "height", "birthplace", "latest_world_rank", "highest_world_rank"]
+    with open('./data/player_table.csv' ,'w', encoding='utf-8') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(player_header)
+        writer.writerows(player_table)
 
 if __name__ == '__main__':
     main()
